c03609cd8597be5f07a6ed3e68b22257fcff2ff9
Tom Dolan <tgdolan@gmail.com>
2014-03-11 06:17:11 -0400

    Cleaning up processing for "User" resource.
    --removed user_sql.clj
    --modified resource/user.clj to refer to data/user.clj
    
    --Tom

--- src/kilo/resources/user.clj
+++ src/kilo/resources/user.clj
@@ -2,9 +2,7 @@
   (:require
             [clojure.data.json :as json]
             [liberator.core :refer [resource defresource]]
-            [kilo.sqldb :as k-sqldb]
-            [kilo.data.user-sql :as k-usersql] 
-            [clojure.pprint :refer (pprint)]))
+            [kilo.data.user :as k-user]))
 
 (defresource
   ^{:doc "This resource provides data for a given user. Utilizing Liberator pattern of
@@ -16,7 +14,7 @@
   :allowed-methods [:get] 
   :available-media-types ["application/json" "application/edn"]
   :exists? (fn [ctx]
-             (if-let [user (k-usersql/get-user-data id)]
+             (if-let [user (k-user/get-user-data id)]
                {::user user}))
   :handle-ok (fn [ctx]
                (::user ctx)))
@@ -26,7 +24,7 @@
   :allowed-methods [:put]
   :available-media-types ["application/json" "application/edn"]
   :put! (fn [ctx]
-          (k-usersql/set-user-data id (json/read-str  (slurp (get-in ctx [:request :body])) :key-fn keyword) ) )
+          (k-user/set-user-data id (json/read-str  (slurp (get-in ctx [:request :body])) :key-fn keyword) ) )
   :respond-with-entity? true
   :handle-ok (fn [ctx]
                (prn "in handle-ok"))
