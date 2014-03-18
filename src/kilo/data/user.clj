42ad6b1b05904ec0c2a27aa6648c3bac9ba5a55d
Tom Dolan <tgdolan@gmail.com>
2014-03-17 20:36:56 -0400

    Kilo : User resource : Adding .clj that defines Korma user entity.
    --Tom

---
+++ src/kilo/data/user.clj
@@ -0,0 +1,25 @@
+(ns kilo.data.user
+  
+  (:require [korma.core :refer [defentity table where select update set-fields]]))
+
+(defentity user
+  (table :mdl_sis_user)
+  )
+
+(defn get-user-data
+  [id]
+  ;; need to convert string id to long
+  (let [id (Long/parseLong id)]
+    (select user
+          (where {:id id})))
+   )
+
+(defn set-user-data
+  [id fields]
+  (let [id (Long/parseLong id)]
+    (update user
+            (set-fields fields)
+            (where {:id id})))
+  )
+ 
+
