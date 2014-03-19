3457d30789415f863e49f5f6680e98c75b7303e0
Tom Dolan <tgdolan@gmail.com>
2014-03-19 06:11:31 -0400

    Tests : Added test to confirm firstname value for GET User
    Also, added utility method to pull first JSON object from
    response body.
    
    Attempted to doc tests by wrapping assertions in calls to clojure.test
    "testing" function, but did not see corresponding messages being
    'printed' at the console.
    
    --Tom

--- test/kilo/api_test.clj
+++ test/kilo/api_test.clj
@@ -6,12 +6,21 @@
             [ring.mock.request :as rmr]
             ))
 
+(defn get-json-object
+  "Utility method for parsing the escaped :body value in http response. Returns first JSON object."
+  [response-body]
+  (-> response-body
+      json/read-str
+      first))
+
 ;; api-test-config makes the mock ring requests to the resources
-;; defined in web.clj. The resulting vars are available for testing here.
+;; defined in web.clj. The resulting vars are available for testing
+;; here.
+
 (use-fixtures :once api-test-config) 
 
 (deftest get-user-test
-  (is (= (:status get-user) 200)))
+  (is (= (:status get-user) 200) "http status 200"))
 
 (deftest verify-user-content-type
   ;; keyed-headers seems like it should be Ring middleware
@@ -20,6 +29,12 @@
       (for [[k v] (:headers get-user)]
         [(keyword k) v])))
   (is (= (:Content-Type keyed-headers) "application/json;charset=UTF-8")))
+
+(deftest verify-user-name
+  (let [user-data
+       (get-json-object (:body get-user))]
+  (is (= (get user-data "firstname") "Anne"))))
+
 
 
 
--- test/kilo/api_test_config.clj
+++ test/kilo/api_test_config.clj
@@ -5,16 +5,15 @@
             [kilo.db-config :as k-testdb]))
 
 (defn set-up-test []
-  ;; this is where we would also initialize and populate test db
 
   ;; the following vars containing the various responses are available to the
-  ;; tests defined in web_test.clj
+  ;; tests defined in api_test.clj
 
   (k-testdb/setup-test-db!)
   (k-testdb/set-korma-connection!)
-  ;;(def onboarding-response (a-web/onboarding (rmr/request :get "/")))
   (def get-user (k-web/app (rmr/request :get "/kilo/user/99999")))
   (def get-group (k-web/app (rmr/request :get "/kilo/group/33333"))))
+
 (defn teardown-test []
   (k-testdb/teardown-test-db!)
   )
