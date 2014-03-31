0d494fb7cfd3293e29f18221a8924d2ff4e2764c
Matt Oquist <moquist@majen.net>
2014-03-31 17:11:03 -0400

    reformat for conventional style, and refer to fns without the reader macro

--- src/kilo/messaging/listener.clj
+++ src/kilo/messaging/listener.clj
@@ -1,21 +1,18 @@
 (ns kilo.messaging.listener
-  (:require
-    [clojure.data.json :as json]
-    [immutant.messaging :as msg]
-    [kilo.data.resource-accessor :as k-resource]))
+  (:require [clojure.data.json :as json]
+            [immutant.messaging :as msg]
+            [kilo.data.resource-accessor :as k-resource]))
 
 (def entity-functions
   "Maps incoming 'operation' value with a function that implements
    analagous operation via Korma db functions."
-  {
-   :put #'k-resource/set-data
-   :get #'k-resource/get-data})
+  {:put k-resource/set-data
+   :get k-resource/get-data})
 
 (defn process-message
   "Receives message from queue and assembles call to the function that will
    either retrieve data from the db or update data in the db (depending on
-   'operation' value in message.
-"
+   'operation' value in message."
   [message]
   (let [parsed-message (json/read-str message :key-fn keyword)
         header (:header parsed-message)
